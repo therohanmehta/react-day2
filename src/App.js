8786d757df744e016ba991e98115687b21fd51c3
therohanmehta <rohandhn786@gmail.com>
2023-03-06 23:24:32 +0530

    React day 2 - Challenge 2

--- src/App.js
+++ src/App.js
@@ -1,17 +1,37 @@
 import logo from './logo.svg';
 import './App.css';
+import { Fragment } from 'react';
 
-
+function Head1(){
+  return(
+    <h1>I am heading one ... from first component</h1>
+  )
+}
+function Head2(){
+  return(
+    <h2>I am heading two ... from second component</h2>
+  )
+}
+function Head3(){
+  return(
+    <h3>I am heading three ... from third component</h3>
+  )
+}
+function Btn(){
+  return(
+    <button>Click Me !!</button>
+  )
+}
 
 
 function App() {
   return (
-    
-    <h1>I am heading one ... from first component</h1>
-    <h2>I am heading two ... from second component</h2>
-    <h3>I am heading three ... from third component</h3>
-    <button>Click Me!!</button>
-    
+    <Fragment>
+      <Head1/>
+      <Head2/>
+      <Head3/>
+      <Btn/>
+    </Fragment>
   )
   }
 
