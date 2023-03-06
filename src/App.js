7099b4a2db1f59d97dd9e91e16d13f953dc0b2ef
therohanmehta <rohandhn786@gmail.com>
2023-03-06 22:54:51 +0530

    Assignment React Day 2 First Part

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+
+
+
+function App() {
+  return (
+    
+    <h1>I am heading one ... from first component</h1>
+    <h2>I am heading two ... from second component</h2>
+    <h3>I am heading three ... from third component</h3>
+    <button>Click Me!!</button>
+    
+  )
+  }
+
+
+export default App;
